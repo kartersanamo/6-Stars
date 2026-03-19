60d6d4f53eb2032a814ba62a56bfe92b3c3f60da
Nolan Schirripa <nv.schirripa@gmail.com>
2026-03-19 12:20:44 -0500

    made UI page for account creation page

--- SEIGroupProject/src/main/java/com/sixstars/ui/CreateAccountPage.java
+++ SEIGroupProject/src/main/java/com/sixstars/ui/CreateAccountPage.java
@@ -7,24 +7,103 @@ import static javax.swing.text.StyleConstants.setBackground;
 
 public class CreateAccountPage extends JPanel {
     public CreateAccountPage(JPanel pages, CardLayout cardLayout) {
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setLayout(new BorderLayout());
         setBackground(Color.WHITE);
 
-        JLabel label = new JLabel("Create Account Page");
-        label.setFont(new Font("Times New Roman", Font.BOLD, 25));
-        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.setBackground(Color.WHITE);
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(30, 50, 30, 50));
 
+        JLabel titleLabel = new JLabel("Create Guest Account");
+        titleLabel.setFont(new Font("Times New Roman", Font.BOLD, 28));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        mainPanel.add(titleLabel);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 30)));
+
+        JPanel formPanel = new JPanel(new GridLayout(6, 2, 10, 15));
+        formPanel.setBackground(Color.WHITE);
+        formPanel.setMaximumSize(new Dimension(500, 250));
+
+        JLabel firstNameLabel = new JLabel("First Name:");
+        JTextField firstNameField = new JTextField();
+
+        JLabel lastNameLabel = new JLabel("Last Name:");
+        JTextField lastNameField = new JTextField();
+
+        JLabel emailLabel = new JLabel("Email:");
+        JTextField emailField = new JTextField();
+
+        JLabel phoneLabel = new JLabel("Phone Number:");
+        JTextField phoneField = new JTextField();
+
+        JLabel usernameLabel = new JLabel("Username:");
+        JTextField usernameField = new JTextField();
+
+        JLabel passwordLabel = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField();
+
+        formPanel.add(firstNameLabel);
+        formPanel.add(firstNameField);
+
+        formPanel.add(lastNameLabel);
+        formPanel.add(lastNameField);
+
+        formPanel.add(emailLabel);
+        formPanel.add(emailField);
+
+        formPanel.add(phoneLabel);
+        formPanel.add(phoneField);
+
+        formPanel.add(usernameLabel);
+        formPanel.add(usernameField);
+
+        formPanel.add(passwordLabel);
+        formPanel.add(passwordField);
+
+        mainPanel.add(formPanel);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 25)));
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 0));
+        buttonPanel.setBackground(Color.WHITE);
+
+        JButton createButton = new JButton("Create Account");
         JButton backButton = new JButton("Back");
-        backButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        backButton.addActionListener(e -> {
-            cardLayout.show(pages, "welcome");
+        buttonPanel.add(createButton);
+        buttonPanel.add(backButton);
+
+        mainPanel.add(buttonPanel);
+
+        add(mainPanel, BorderLayout.CENTER);
+
+        createButton.addActionListener(e -> {
+            String firstName = firstNameField.getText();
+            String lastName = lastNameField.getText();
+            String email = emailField.getText();
+            String phone = phoneField.getText();
+            String username = usernameField.getText();
+            String password = new String(passwordField.getPassword());
+
+            JOptionPane.showMessageDialog(
+                    this,
+                    "Guest account created for " + firstName + " " + lastName,
+                    "Success",
+                    JOptionPane.INFORMATION_MESSAGE
+            );
+
+            firstNameField.setText("");
+            lastNameField.setText("");
+            emailField.setText("");
+            phoneField.setText("");
+            usernameField.setText("");
+            passwordField.setText("");
         });
 
-        add(Box.createVerticalGlue());
-        add(label);
-        add(Box.createRigidArea(new Dimension(0, 20)));
-        add(backButton);
-        add(Box.createVerticalGlue());
+        backButton.addActionListener(e -> {
+            CardLayout cl = (CardLayout) pages.getLayout();
+            cl.show(pages, "welcome");
+        });
     }
 }
